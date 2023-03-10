530e269a1a9a575c0ed1eaad0b16054e6958a555
Kudums <kudumsendijs@gmail.com>
2023-03-10 09:01:18 +0200

    Izveidota lietotājam izvēlēties darbības

--- src/kudums_pica/Ceptuve.java
+++ src/kudums_pica/Ceptuve.java
@@ -1,10 +1,26 @@
 package kudums_pica;
 
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 public class Ceptuve {
 
 	public static void main(String[] args) {
+		String izvele;
 		
-
+		String[] darbibas = {"Pasūtīt picu","Apskatīties pasūtījumus","Izcept picu","Beigt darbu"};
+		
+		ArrayList<Object> pica = new ArrayList<>();
+		
+		do{
+			izvele = (String)JOptionPane.showInputDialog(null,"Izvēlies darbību","Darbības izvēle"
+					,JOptionPane.QUESTION_MESSAGE,null,darbibas,darbibas[0]);
+			
+			
+			
+		}while(!izvele.equalsIgnoreCase("Beigt darbu"));
+		
 	}
 
 }
