59afd21a26205145f7e89afd8fa70ef9272a95ca
Kudums <kudumsendijs@gmail.com>
2023-03-10 08:54:11 +0200

    Izveidots konstruktors ar picas atribūtiem

--- src/kudums_pica/Pica.java
+++ src/kudums_pica/Pica.java
@@ -2,4 +2,16 @@ package kudums_pica;
 
 public class Pica {
 
+	//Attribūti
+	private String tips;		
+	private String merce;
+	private int cena;
+	private int izmeres;
+	private boolean piegade;			//ja true = piegadā picu uz adresu false = no ceptuves savakts
+	private int skaits;
+
+	
+	
+	//Metodes
+	//....
 }
